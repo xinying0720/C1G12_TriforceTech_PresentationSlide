17eeef2457872056bd006282dce7bd65b6c2bebf
LimJiaqian <limjiaqian5@gmail.com>
2025-12-05 14:12:53 +0800

    team

--- src/CircularGallery.jsx
+++ src/CircularGallery.jsx
@@ -365,19 +365,11 @@ class App {
 
   createMedias(items, bend = 1, textColor, borderRadius, font) {
     const defaultItems = [
-      { image: `https://picsum.photos/seed/1/800/600?grayscale`, text: 'Grandma' },
-      { image: `https://picsum.photos/seed/2/800/600?grayscale`, text: 'Desk Setup' },
-      { image: `https://picsum.photos/seed/3/800/600?grayscale`, text: 'Waterfall' },
-      { image: `https://picsum.photos/seed/4/800/600?grayscale`, text: 'Strawberries' },
-      { image: `https://picsum.photos/seed/5/800/600?grayscale`, text: 'Deep Diving' },
-      { image: `https://picsum.photos/seed/16/800/600?grayscale`, text: 'Train Track' },
-      { image: `https://picsum.photos/seed/17/800/600?grayscale`, text: 'Santorini' },
-      { image: `https://picsum.photos/seed/8/800/600?grayscale`, text: 'Blurry Lights' },
-      { image: `https://picsum.photos/seed/9/800/600?grayscale`, text: 'New York' },
-      { image: `https://share.google/images/whO1CCXn6GymIVJp0`, text: 'Good Boy' },
-      { image: `https://picsum.photos/seed/21/800/600?grayscale`, text: 'Coastline' },
-      { image: `https://picsum.photos/seed/12/800/600?grayscale`, text: 'Palm Trees' }
-
+      { image: `/team/felicia.png`, text: 'Felicia Sia Xin Rou' },
+      { image: `/team/jiaqian.png`, text: 'Lim Jia Qian' },
+      { image: `/team/jiahui.png`, text: 'Ooi Jia Hui' },
+      { image: `/team/jiaxin.png`, text: 'Beh Jia Xin' },
+      { image: `/team/xinying.png`, text: 'Lim Xin Ying' }
     ];
     const galleryItems = items && items.length ? items : defaultItems;
     this.mediasImages = galleryItems.concat(galleryItems);
@@ -385,7 +377,7 @@ class App {
       return new Media({
         geometry: this.planeGeometry,
         gl: this.gl,
-        image: data.image,
+        image: data.image,   
         index,
         length: this.mediasImages.length,
         renderer: this.renderer,
@@ -399,7 +391,8 @@ class App {
         font
       });
     });
-  }
+}
+
 
   /* --- center initial scroll so the gallery is not shoved to the left --- */
   centerInitialScroll() {
